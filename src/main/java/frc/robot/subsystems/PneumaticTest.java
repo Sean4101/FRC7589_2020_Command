6fa86af87ed80ff9bdb909e11026bcd69a4b0e53
sean4101 <seanchen1014@gmail.com>
2020-09-05 20:12:06 +0800

    Pheonix vendor app added

--- src/main/java/frc/robot/subsystems/PneumaticTest.java
+++ src/main/java/frc/robot/subsystems/PneumaticTest.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
